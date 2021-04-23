844bab0b10a0523ee27d324c43ee33035cd2c99a
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-04-22 20:09:23 -0400

    created profile in api

--- codrs/models.py
+++ codrs/models.py
@@ -8,3 +8,11 @@ class Push(models.Model):
     user = models.CharField(max_length=100)
     push = models.CharField(max_length=1000)
     # array = models.ForeignKey(Array, on_delete=models.CASCADE, related_name="pushes")
+
+class Profile(models.Model):
+    firstname = models.CharField(max_length=100, default='')
+    lastname = models.CharField(max_length=100, default='')
+    genderpronouns = models.CharField(max_length=100, default='')
+    location = models.CharField(max_length=100, default='')
+    aboutme = models.CharField(max_length=500, default='')
+    linkedin = models.CharField(max_length=100, default='')
